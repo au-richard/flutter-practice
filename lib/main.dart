18ed360dd4451ae07df900eac1579426a3709e7d
Richard Au <au.richard4@gmail.com>
2023-01-05 14:47:31 -0800

    Adding bottom nav bar.

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +37,17 @@ class _RootPageState extends State<RootPage> {
           debugPrint("Floating Action Button");
         },
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
       ),
     );
   }
