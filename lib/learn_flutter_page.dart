89e53d8bf886c7ca84d31f75d72e8b8a66c73ed3
Richard Au <au.richard4@gmail.com>
2023-01-05 15:31:22 -0800

    Adding einstein image, 2 buttons to learn flutter page.

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -21,6 +21,37 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
+      body: Column(
+        children: [
+          Image.asset("images/einstein.jpg"),
+          // const SizedBox(
+          //   height: 10,
+          // ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            width: double.infinity,
+            color: Colors.blueGrey,
+            child: const Center(
+              child: Text(
+                "This is a text widget",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint("Elevated Button");
+            },
+            child: const Text("Elevated Button"),
+          ),
+        ],
+      ),
     );
   }
 }
