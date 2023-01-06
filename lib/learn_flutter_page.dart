7565ad02a48f7ded23084c8d3968e9a941f1b2a5
Richard Au <au.richard4@gmail.com>
2023-01-05 16:11:27 -0800

    Making learn flutter page scrollable.

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -23,89 +23,104 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint("Actions");
+            },
+            icon: const Icon(
+              Icons.info_outline,
+            ),
+          ),
+        ],
       ),
-      body: Column(
-        children: [
-          Image.asset(
-            "images/einstein.jpg",
-            height: 150,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            width: double.infinity,
-            color: Colors.blueGrey,
-            child: const Center(
-              child: Text(
-                "This is a text widget",
-                style: TextStyle(
-                  color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "images/einstein.jpg",
+              height: 150,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              width: double.infinity,
+              color: Colors.blueGrey,
+              child: const Center(
+                child: Text(
+                  "This is a text widget",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: isSwitch ? Colors.green : Colors.blue,
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint("Elevated Button");
+              },
+              child: const Text("Elevated Button"),
             ),
-            onPressed: () {
-              debugPrint("Elevated Button");
-            },
-            child: const Text("Elevated Button"),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint("Outline Button");
-            },
-            child: const Text("Outlined Button"),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint("Text Button");
-            },
-            child: const Text("Text Button"),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint("This is the row.");
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.blue,
-                ),
-                Text("Row Widget"),
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.blue,
-                ),
-              ],
+            OutlinedButton(
+              onPressed: () {
+                debugPrint("Outline Button");
+              },
+              child: const Text("Outlined Button"),
             ),
-          ),
-          Switch(
-              value: isSwitch,
-              onChanged: (bool newBool) {
-                setState(() {
-                  isSwitch = newBool;
-                });
-              }),
-          Checkbox(
-              value: isCheckBox,
-              onChanged: (bool? newBool) {
-                setState(() {
-                  isCheckBox = newBool;
-                });
-              })
-        ],
+            TextButton(
+              onPressed: () {
+                debugPrint("Text Button");
+              },
+              child: const Text("Text Button"),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint("This is the row.");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text("Row Widget"),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckBox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckBox = newBool;
+                  });
+                }),
+            Image.network(
+              "https://upload.wikimedia.org/wikipedia/en/8/86/Einstein_tongue.jpg",
+            ),
+          ],
+        ),
       ),
     );
   }
